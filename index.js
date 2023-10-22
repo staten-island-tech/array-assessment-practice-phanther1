d9ac8b473f2bd2c374357833a310df600d1d4ea5
Karl F <144461089+phanther1@users.noreply.github.com>
2023-10-22 11:22:14 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const books = [
   },
   {
     name: "Harry Potter and the Deathly Hallows",
-    authorFirst: "JK",
+    authorFirst: "J.K.",
     authorLast: "Rowling",
     publishDate: 1997,
     genre: ["fantasy", , "fiction"],
@@ -87,41 +87,74 @@ const titles = [
 //Array of authors and the book they wrote
 //"--- wrote --- in ---"
 
-/* books.forEach((books) => {
-  console.log(books.authorFirst + " " + books.authorLast + " wrote " + books.name + " in " + books.publishDate);
-}); */
+const authorsAndBooks = books.map((book) => {         //maps the book to author
+  const author = `${book.authorFirst} ${book.authorLast}`;      //lists author name
+  return `${author} wrote ${book.name} in ${book.publishDate}`;   //prints out
+});
+console.log (authorsAndBooks);
 
 //Sort books from oldest to most recent
 
-/* books.sort((a,b) => (a.publishDate - b.publishDate)); */
+const dateSort = books.sort((a,b) => a.publishDate - b.publishDate); 
+console.log(dateSort);
 
 //sort books alphabetically
 
-books.sort(function(a, b) {
-  if (a.name < b.name) {
-    return -1;
-  }
-  if (a.name > b.name) {
-    return 1;
-  }
-  return 0;
-}
-console.log(books));
+/* const alphabetSort = books.sort((a,b) => a.name.localeCompare(b.name));      //sorts comparing the name (locally)
+console.log (alphabetSort); */
+
 //Find who wrote War and Peace
 
+const authorWritten = "War and Peace" //determine which book
+const findAuthor = books.find((author) => author.name === authorWritten);   //find the author that wrote the book
 
+if (authorWritten) {
+const authorOfBook = `${findAuthor.authorFirst} ${findAuthor.authorLast}`;  //prints the author name
+console.log(`The author of War and Peace is ${authorOfBook}`);
+}
+else {
+  console.log("There is no such book") //if no book exists
+}
 
 //how many books were written before 1900?
 
-
+const bookDate = books.filter((book) => book.publishDate < 1900); 
+const countBookDate = bookDate.length;
+console.log (`# of books written beofre 1900: ${countBookDate}`);
 
 //was there at least one book published within the last 100 years?
 
+const currrentYear = new Date().getFullYear();
+const last100Years = currrentYear - 100;
 
+const books100 = books.some((book) => book.publishDate >= last100Years && book.publishDate <= currrentYear);
+
+if (books100) {
+  console.log ("Yes, ther is books published within da last 100 years");
+}
+else {
+  console.log ("Nah, all da books published here are older than 100 yrs");
+}
 
 //was every book published within the last 100 years?
 
+const last100 = books.every((book) => book.publishDate >= last100Years && book.publishDate <= currrentYear);
 
+if (last100) {
+  console.log ("Yes sir, all da book within the last 100 years of published");
+}
+else {
+  console.log ("No way, ofc thers books published more than 100 yrs ago here");
+}
 
 //print a list of books that "includes" the genre historical
 
+const history = books.filter((type) => type.genre.includes("historical"));
+if (history.length > 0) {
+const historyTitles = history.map(book => book.name);
+console.log(`Books that have the 'historical' genre: ${historyTitles}`);
+}
+
+else {
+  console.log("Thers no book that has 'historical' genre")
+}
